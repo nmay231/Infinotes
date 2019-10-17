a2e181d33b85a6e1743e0d73d8554e7a8387a7fd
Noah May <noahmouse2011@gmail.com>
2019-10-17 09:50:49 -0600

    Prepared redux devtools for prod

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
